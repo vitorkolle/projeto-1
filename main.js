d3b4d752ed0570fc82c5655d9c92eb14f7855b92
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2024-01-30 12:19:25 -0300

    criação dos cards

--- main.js
+++ main.js
@@ -3,9 +3,17 @@
 const btnCadastrar = document.getElementById('botao-cadastrar')
 btnCadastrar.addEventListener('click', cadastrar)
 
+const btnLogar = document.getElementById('botao-logar')
+btnLogar.addEventListener('click', logar)
+
 
 function cadastrar(){
     const nome = document.getElementById('input-nome').value
+    const email = document.getElementById('input-email').value
+    const senha = document.getElementById('input-senha').value
+}
+
+function logar(){
     const email = document.getElementById('input-email').value
     const senha = document.getElementById('input-senha').value
 }
