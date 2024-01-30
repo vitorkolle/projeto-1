c30a262876ffe66d34676ba6911cffb087c5a599
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2024-01-30 12:10:30 -0300

    começo do projeto

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const btnCadastrar = document.getElementById('botao-cadastrar')
+btnCadastrar.addEventListener('click', cadastrar)
+
+
+function cadastrar(){
+    const nome = document.getElementById('input-nome').value
+    const email = document.getElementById('input-email').value
+    const senha = document.getElementById('input-senha').value
+}
