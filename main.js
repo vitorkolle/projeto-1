577f6e681b38af653e632497b29de9be9d65cc90
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2024-02-06 12:26:44 -0300

    cadastro em andamento

--- main.js
+++ main.js
@@ -1,19 +1,57 @@
-'use strict'
+'use strict' 
 
-const btnCadastrar = document.getElementById('botao-cadastrar')
-btnCadastrar.addEventListener('click', cadastrar)
+const nome = document.getElementById('input-nome').value
+const email = document.getElementById('input-email').value
+const senha = document.getElementById('input-senha').value
 
-const btnLogar = document.getElementById('botao-logar')
-btnLogar.addEventListener('click', logar)
+//Uso da detecção da tecla 'enter' para realizar a ação
+const input_nome = document.getElementById('input-nome')
+    input_nome.addEventListener('keypress', function(event){
+        if(event.key == 'Enter'){
+            criarNovoUsuario()
+        }
+    })
 
+    const input_email = document.getElementById('input-email')
+    input_email.addEventListener('keypress', function(event){
+        if(event.key == 'Enter'){
+            criarNovoUsuario()
+        }
+    })
 
-function cadastrar(){
-    const nome = document.getElementById('input-nome').value
-    const email = document.getElementById('input-email').value
-    const senha = document.getElementById('input-senha').value
+    const input_senha = document.getElementById('input-senha')
+    input_senha.addEventListener('keypress', function(event){
+        if(event.key == 'Enter'){
+            criarNovoUsuario()
+        }
+    })
+    
+function criarNovoUsuario(){
+const nome = document.getElementById('input-nome').value
+const email = document.getElementById('input-email').value
+const senha = document.getElementById('input-senha').value
+
+        const novoUsuario = {
+            nome: nome,
+            email: email,
+            senha: senha 
+        }
+    
+        cadastrar(novoUsuario) 
 }
 
-function logar(){
-    const email = document.getElementById('input-email').value
-    const senha = document.getElementById('input-senha').value
+async function cadastrar(novoUsuario){
+    
+    const url = 'http://localhost:5080/usuario'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type:' : 'application/json',
+        },
+        body: JSON.stringify(novoUsuario)
+    }
+
+    const response = await fetch(url, options)
+    console.log(response.ok)
+    return response.ok
 }
