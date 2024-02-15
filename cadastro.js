35f60324347e8227e743c32fa8d7ebb4038da52c
VITOR PAES KOLLE <23111830@CFP127ALUNO.LOCAL>
2024-02-15 15:00:25 -0300

    continuação

---
+++ cadastro.js
@@ -0,0 +1,42 @@
+'use strict' 
+
+function criarNovoUsuario(){
+const nome = document.getElementById('input-nome').value
+const email = document.getElementById('input-email').value
+const senha = document.getElementById('input-senha').value
+
+if(nome === '' || senha === '' || email === ''){
+    alert('Por favor, preencha todos os campos!!')
+    return false;
+}
+try{
+        const novoUsuario = {
+            nome: nome,
+            email: email,
+            senha: senha 
+        }
+    
+        cadastrar(novoUsuario) 
+}
+catch(error){
+    alert('Erro ao acessar a API !')
+    console.error(error);
+}
+}
+
+
+async function cadastrar(novoUsuario){
+    
+    const url = 'http://localhost:5080/usuario'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type:' : 'application/json',
+        },
+        body: JSON.stringify(novoUsuario)
+    }
+
+    const response = await fetch(url, options)
+    console.log(response.ok)
+    return response.ok
+}
--- pag login/login.js
+++ pag login/login.js
@@ -1,2 +1,34 @@
 'use strict'
 
+async function validarLogin(){
+
+    const email = document.getElementById('input-email').value;
+    const senha = document.getElementById('input-senha').value; 
+        
+    if(email === '' || senha === ''){
+        alert('Por favor, preencha todos os campos!!')
+        return false;
+    } else {
+
+    try {
+
+        const users = await fetch('http://localhost:5080/usuario');
+
+        const listUsers = await users.json();
+        
+        console.log(listUsers)
+        listUsers.forEach((user) => {
+            if(email === user.email && senha === user.senha){
+                alert('Usuário Logado com Sucesso !!');
+                window.location.href = "../pag home/home.html"
+                return true;
+            }
+        })
+
+    } catch (error) {
+        alert('Erro ao acessar a API !')
+        console.error(error);
+    }
+
+    }
+}
